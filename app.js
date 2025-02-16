8873f13183a95f3fa5b74c5893f533b6c960bc60
Elai <emyllematos7@gmail.com>
2025-02-16 13:07:25 -0300

    add documentacao das funcoes

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaNomes = [];
 
+/**
+ * Adiciona um nome à lista de amigos.
+ * 
+ * Esta função obtém o valor do campo de entrada com o ID 'amigo'. 
+ * Se o campo estiver vazio, exibe um alerta solicitando que o usuário insira um nome.
+ * Caso contrário, adiciona o nome à lista de amigos, limpa o campo de entrada e atualiza a exibição da lista.
+ */
 function adicionarAmigo() {
     let nome = document.getElementById('amigo').value;
 
@@ -14,6 +21,10 @@ function adicionarAmigo() {
     }
 }
 
+/**
+ * Função para exibir a lista de amigos na página.
+ * Limpa o conteúdo atual da lista e adiciona cada nome da lista de nomes como um item de lista (li).
+ */
 function mostrarLista() {
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = "";
@@ -22,6 +33,10 @@ function mostrarLista() {
     }
 }
 
+/**
+ * Sorteia um amigo da lista de nomes e exibe o resultado na página.
+ * Se a lista estiver vazia, exibe um alerta informando que nenhum amigo foi adicionado.
+ */
 function sortearAmigo() {
     if (listaNomes.length == 0) {
         alert('Nenhum amigo adicionado.');
